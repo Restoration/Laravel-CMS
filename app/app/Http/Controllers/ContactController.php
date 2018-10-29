9f685ca7548a865b67c9ff4953375d9bba2fadc3
RyotArch <bboy.ryu@icloud.com>
2018-10-29 02:52:27 -0700

    [Add] Validate
    Add contact validation

--- app/app/Http/Controllers/ContactController.php
+++ app/app/Http/Controllers/ContactController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Extension\Validate;
 
 class ContactController extends Controller
 {
     public function index() {
         return view('contact.index');
     }
-    public function send() {
+    public function send(Request $request) {
+        $validate = new Validate;
+        $errors =  $validate->validateContact($request);
+        if(!empty($errors)){
+            return view('contact.index', ['errors' => $errors]);
+        }
         return view('contact.complete');
     }
 
